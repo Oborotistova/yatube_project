c72b61eace05d881b4eade64998a6648ad27a6a0
Ольга Хомутова <homutova.email@gmail.com>
2022-03-25 21:41:47 +0500

    for tests

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:group_id>/', views.group_posts)
+    path('', views.index, name = 'index'),
+    path('group/<slug:group_id>/', views.group_posts, name = 'group_posts')
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
-def index(request):    
-    return HttpResponse('Главная страница')
+from .models import Post, Group
 
-def group_posts(request, group_id):
-    return HttpResponse('Работает')
+
+# Create your views here.
+# def index(request):
+#     template = 'posts/index.html'
+#     title = 'Это главная страница проекта Yatube'
+#     context = {
+#         # В словарь можно передать переменную
+#         'title': title
+        
+#     }
+#     return render(request, template, context)
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+
+    context = {
+        # В словарь можно передать переменную
+        'group': group,
+        'posts': posts,
+      
+    }
+    return render(request, 'posts/group_list.html', context)
